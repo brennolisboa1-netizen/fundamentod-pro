77ef55b3263b055269ac505b3c96e9c2c27ba389
brenno lisboa <brennolisboa11@gmail.com>
2026-04-01 09:16:45 -0300

    exercicio 01/04

--- estrututas_pizzaria.py
+++ estrututas_pizzaria.py
@@ -1,6 +1,6 @@
 # variavéis da pizzaria
 pizza_sabor =input("informe o sabor da pizza - [frango com requeijão],[calabresa],[mussarela],[banana com chocolate]:")
-pizza_tamanho =input ("informe o tamanho da pizza - [peqeuna],[média],[grande]: ")
+(())
 dia_semana =input ("informe o dia da semana - [quarta], [quinta], [sábado],[domingo]: ")
 
 print (f"o sabor escolhido da pizza é {pizza_sabor}, o tamanho da pizza é {pizza_tamanho}, e hoje é {dia_semana}.")
